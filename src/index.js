fdd0a715de1bc34d74c95815f7bc886f8d07c11d
unknown <lubost2@gmail.com>
2019-12-15 23:15:30 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ArButton from './components/ArButton';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
