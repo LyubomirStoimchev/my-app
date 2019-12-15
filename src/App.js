78d799b2e9821663a2eba6019744e90d53c0332e
unknown <lubost2@gmail.com>
2019-12-15 12:24:06 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { showPopup: false };
+    }
+
+    togglePopup() {
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
+    }
+
     render() {
         return (
             <div className="App">
@@ -12,6 +24,17 @@ class App extends React.Component {
                     </a-anchor>
                     <a-camera-static preset="hiro" />
                 </a-scene>
+
+                <h1> Simple Popup Example In React Application </h1>
+                <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>
+
+                {this.state.showPopup ?
+                    <Popup
+                        text='Click "Close Button" to hide popup'
+                        closePopup={this.togglePopup.bind(this)}
+                    />
+                    : null
+                }
             </div>
         );
     }
