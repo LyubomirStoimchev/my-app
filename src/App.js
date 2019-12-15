17758e3d4515bbb3850c602ce305000286c565ed
unknown <lubost2@gmail.com>
2019-12-15 23:04:15 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React from 'react';
-// import './App.css';
-import Popup from './components/Popup'
+import './App.css';
+import ArButton from './components/ArButton';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { showPopup: false };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         showPopup: false,
+    //         message: ''
+    //     };
+    // }
 
-    togglePopup() {
-        this.setState({
-            showPopup: !this.state.showPopup
-        });
-    }
+    // togglePopup() {
+    //     this.setState({
+    //         showPopup: !this.state.showPopup,
+    //         message: ''
+    //     });
+
+    //     // console.log(this.state);
+    // }
 
     render() {
         return (
             <div>
-                <h1> Simple Popup Example In React Application </h1>
-                <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>
+                {/* // <div className="App"> */}
+                <a-scene embedded arjs="sourceType: webcam;" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" aframe-inspector-removed-embedded="undefined">
+                    <canvas class="a-canvas" data-aframe-canvas="true" width="300" height="150" />
 
-                {
-                    this.state.showPopup ?
-                        <Popup
-                            text='Click "Close Button" to hide popup'
-                            closePopup={this.togglePopup.bind(this)}
-                        />
-                        : null
-                }
+                    <a-anchor hit-testing-enabled="true">
+                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
+                        <a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+
+                    {/* <ArButton /> */}
+
+                </a-scene>
             </div>
         );
     }
---
+++ src/components/ArButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ArButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            position: "",
+            href: "",
+            assetId: "",
+            assetSrc: ""
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <a-assets>
+                    <img crossorigin="anonymous" id={this.state.assetId} src={this.state.assetSrc} />
+                </a-assets>
+
+                <a-link class="clickable" href={this.state.href} title="" image="" position={this.state.position} rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:.">
+                    <a-box scale="0.8 0.8 1.05" material={"src:#" + this.state.assetId} position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear" />
+                    </a-box>
+                </a-link>
+            </div>
+        );
+    }
+}
+
+export default ArButton;
