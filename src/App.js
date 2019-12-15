13a702bf4b186df0e071fb55dd195d90ece4cb99
unknown <lubost2@gmail.com>
2019-12-15 13:55:23 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import Popup from './components/Popup'
 
 class App extends React.Component {
 
@@ -16,24 +17,17 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <a-scene arjs='sourceType: webcam; debugUIEnabled: false'>
-                    <a-anchor hit-testing-enabled="true">
-                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
-                        {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
-                    </a-anchor>
-                    <a-camera-static preset="hiro" />
-                </a-scene>
-
+            <div>
                 <h1> Simple Popup Example In React Application </h1>
                 <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>
 
-                {this.state.showPopup ?
-                    <Popup
-                        text='Click "Close Button" to hide popup'
-                        closePopup={this.togglePopup.bind(this)}
-                    />
-                    : null
+                {
+                    this.state.showPopup ?
+                        <Popup
+                            text='Click "Close Button" to hide popup'
+                            closePopup={this.togglePopup.bind(this)}
+                        />
+                        : null
                 }
             </div>
         );
