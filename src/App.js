4d0a5db8fbc7fd3aec035316d1c04950187e4d75
unknown <lubost2@gmail.com>
2019-12-17 00:34:14 +0200

    asdda

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <a-scene embedded arjs="sourceType: webcam;" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" aframe-inspector-removed-embedded="undefined"><canvas className="a-canvas" data-aframe-canvas="true" width="300" height="150"></canvas>
-                <a-assets>
+                <a-scene embedded arjs="sourceType: webcam;" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" aframe-inspector-removed-embedded="undefined">
+                    <canvas className="a-canvas" data-aframe-canvas="true" width="300" height="150">
+                    </canvas>
+                    <a-assets>
                         <img crossOrigin="anonymous" id="skypeTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostSkype.png?1528821334003" />
                         <img crossOrigin="anonymous" id="phoneTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostPhone.png?1528821333458" />
                         {/* <img crossorigin="anonymous" id="messageTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostMessages.png?1528821333263">
@@ -37,9 +39,9 @@ class App extends React.Component {
                                                         <img crossorigin="anonymous" id="headshotTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostPostHeadshot.png?1528821994851">   */}
                     </a-assets>
 
-                    <a-marker camera="active:false" preset="hiro" visible="false" material="" arjs-anchor="" arjs-hit-testing="">
+                    <a-marker data-camera="active:false" preset="hiro" visible="false" material="" arjs-anchor="" arjs-hit-testing="">
 
-                        <a-entity id="stars" particle-system="preset: default; color: #515; particleCount: 30; maxAge: 2; randomize: false; blending: 1; opacity: .5; velocityValue:'50 10 0'" position="0 0 0" rotation="-90 0 0" direction="-1"></a-entity>
+                        <a-entity id="stars" data-particle-system="preset: default; color: #515; particleCount: 30; maxAge: 2; randomize: false; blending: 1; opacity: .5; velocityValue:'50 10 0'" data-position="0 0 0" data-rotation="-90 0 0" direction="-1"></a-entity>
 
                         {/* <a-entity position="-2 .5 2" rotation="-65.98 -4.56 5.28" scale="0.3 0.39 0.1" text-geometry="bevelEnabled:true;bevelSize:0.05;bevelThickness:0.05;font:https://cdn.glitch.com/c719c986-c0c5-48b8-967c-3cd8b8aa17f3%2FdawningOfANewDayRegular.typeface.json?1490305922844;height:0;size:1;value:Think Outside the Box" material="color:lavenderblush;metalness:1;roughness:0;sphericalEnvMap:https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FneonGreeni.png?1528781747193"></a-entity>
                         <a-entity position="-2 .5 -1.4" rotation="-45.98 -4.56 5.28" scale="0.1 .25 .2" text-geometry="bevelEnabled:true;bevelSize:0.1;bevelThickness:0.1;curveSegments:1;font:https://cdn.glitch.com/c719c986-c0c5-48b8-967c-3cd8b8aa17f3%2Fexo2Black.typeface.json?1490305922150;height:0.5;size:2;value:J O N A T H A N    S C H W A R T Z" material="color:pink;metalness:0.9;roughness:0.05;sphericalEnvMap:https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FTextureMap.png?1528781398531"></a-entity> */}
@@ -52,9 +54,11 @@ class App extends React.Component {
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
                         </a-box></a-link> */}
 
-                        <a-link class="clickable" href="tel:123123123" title="" image="" position="-2 0 -.333" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#phoneTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
-                            <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
-                        </a-box></a-link>
+                        <a-link class="clickable" href="tel:123123123" data-title="" data-image="" data-position="-2 0 -.333" data-rotation="-67.42 0 0" data-scale="0.6 0.6 0.6" data-geometry="primitive:circle;segments:64" data-material="shader:portal;side:double;visible:false" data-link="title:.">
+                            <a-box data-scale="0.8 0.8 1.05" data-material="src:#phoneTexture" data-position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" data-radius="2" data-segments-height="84" data-rotation="-23.450000000000003 0 0" data-geometry="">
+                                <a-animation data-attribute="rotation" data-begin="mouseenter" data-loop="true" data-end="mouseleave" data-dur="2000" data-fill="backwards" data-to="335 360 360" data-ease="linear"></a-animation>
+                            </a-box>
+                        </a-link>
 
                         {/* <a-link class="clickable" href="mailto:jonathanpatrickschwartz@gmail.com" title="" image="" position="-.7 0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#emailTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
@@ -72,9 +76,11 @@ class App extends React.Component {
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
                         </a-box></a-link> */}
 
-                        <a-link class="clickable" href="skype:me?chat" title="" image="" position="2.2 0.0 -.33" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#skypeTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
-                            <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
-                        </a-box></a-link>
+                        <a-link class="clickable" href="skype:me?chat" data-title="" data-image="" data-position="2.2 0.0 -.33" data-rotation="-67.42 0 0" data-scale="0.6 0.6 0.6" data-geometry="primitive:circle;segments:64" data-material="shader:portal;side:double;visible:false" data-link="title:.">
+                            <a-box data-scale="0.8 0.8 1.05" data-material="src:#skypeTexture" data-position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" data-radius="2" data-segments-height="84" data-rotation="-23.450000000000003 0 0" data-geometry="">
+                                <a-animation data-attribute="rotation" data-begin="mouseenter" data-loop="true" data-end="mouseleave" data-dur="2000" data-fill="backwards" data-to="0 360 0" data-ease="linear"></a-animation>
+                            </a-box>
+                        </a-link>
 
                         {/* <a-link class="clickable" href="https://jonathanschwartz.surge.sh/" title="" image="" position=".8 0.0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#websiteTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
@@ -82,9 +88,9 @@ class App extends React.Component {
                     </a-marker>
 
                     <a-entity camera="" position="" rotation="" scale="" visible=""></a-entity>
-                    <a-entity cursor="fuse: true; fuseTimeout: 2000" raycaster="objects: .clickable" position="0 0 -1" scale="0.01 0.01 0.01" geometry="primitive: ring" material="color: white; shader: flat" rotation="" visible="">
-                        <a-animation begin="fusing" easing="ease-in" attribute="scale" fill="backwards" from="0.01 0.01 0.01" to="0.02 0.02 0.02" dur="3000"></a-animation>
-                        <a-animation begin="fusing" easing="ease-in" attribute="material.color" from="white" to="orange" dur="3000"></a-animation>
+                    <a-entity data-cursor="fuse: true; fuseTimeout: 2000" data-raycaster="objects: .clickable" data-position="0 0 -1" data-scale="0.01 0.01 0.01" data-geometry="primitive: ring" data-material="color: white; shader: flat" data-rotation="" data-visible="">
+                        <a-animation data-begin="fusing" data-easing="ease-in" data-attribute="scale" data-fill="backwards" data-from="0.01 0.01 0.01" data-to="0.02 0.02 0.02" data-dur="3000"></a-animation>
+                        <a-animation data-begin="fusing" data-easing="ease-in" data-attribute="material.color" data-from="white" data-to="orange" data-dur="3000"></a-animation>
                     </a-entity>
 
                 </a-scene>
