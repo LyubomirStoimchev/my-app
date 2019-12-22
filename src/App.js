70a8df7fa74b43996e40dd2dbfd1aa54125c218c
Lyubomir Stoimchev <lyubomir.st@sbtech.com>
2019-12-22 19:44:11 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -24,13 +24,12 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <a-scene embedded arjs="sourceType: webcam;" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" aframe-inspector-removed-embedded="undefined">
-                    
-                        <a-marker preset="hiro">
-                            <a-box position='0 0.5 0' material='color: yellow;'></a-box>
-                        </a-marker>
-                        <a-entity camera></a-entity>
-                    
+                <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false'>
+                    <a-anchor hit-testing-enabled="true">
+                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
+                        {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
                 </a-scene>
             </div>
         );
