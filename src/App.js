7df9fd52d5ae0ddf0fbbfbc5ce8130339a171d62
Lyubomir Stoimchev <lyubomir.st@sbtech.com>
2019-12-22 21:50:13 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <a-scene arjs='sourceType: webcam; debugUIEnabled: false'>
+            <div class="App">
+                  <a-scene arjs="sourceType: webcam;" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" aframe-inspector-removed-embedded="undefined">
+                      <canvas class="a-canvas" data-aframe-canvas="true" width="300" height="150"></canvas>
                     <a-assets>
                         <img crossOrigin="anonymous" id="skypeTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostSkype.png?1528821334003" />
                         <img crossOrigin="anonymous" id="phoneTexture" src="https://cdn.glitch.com/6f8b5a13-fd4d-445d-b9eb-57c735d720ea%2FPostPhone.png?1528821333458" />
@@ -51,41 +52,42 @@ class App extends React.Component {
                             segments-height="84">
                         </a-box>
 
-                        <a-link classNameName="clickable" href="https://www.linkedin.com/in/jonathanpschwartz" title="" image="" position="-1.4 0.0 0.333" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#linkedinTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="https://www.linkedin.com/in/jonathanpschwartz" title="" image="" position="-1.4 0.0 0.333" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:.">
+                            <a-box scale="0.8 0.8 1.05" material="src:#linkedinTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="tel:19083766480" title="" image="" position="-2 0 -.333" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#phoneTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="tel:19083766480" title="" image="" position="-2 0 -.333" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#phoneTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="mailto:jonathanpatrickschwartz@gmail.com" title="" image="" position="-.7 0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#emailTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="mailto:jonathanpatrickschwartz@gmail.com" title="" image="" position="-.7 0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#emailTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="https://github.com/filmboy3/" title="" image="" position="-.7 0 -1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#githubTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="https://github.com/filmboy3/" title="" image="" position="-.7 0 -1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#githubTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="335 360 360" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="sms:9083766480" title="" image="" position=".8 0.0 -1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#messageTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="sms:9083766480" title="" image="" position=".8 0.0 -1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#messageTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="https://docs.google.com/document/d/1BdX3pEODMX-oyFSSQNg60VIDMhcEou6NY1sbp1pkp-Q/edit?usp=sharing" position="1.6 0.0 .333" title="" image="" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#resumeTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="https://docs.google.com/document/d/1BdX3pEODMX-oyFSSQNg60VIDMhcEou6NY1sbp1pkp-Q/edit?usp=sharing" position="1.6 0.0 .333" title="" image="" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#resumeTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="skype:jonathan.p.schwartz?chat" title="" image="" position="2.2 0.0 -.33" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#skypeTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="skype:jonathan.p.schwartz?chat" title="" image="" position="2.2 0.0 -.33" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#skypeTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
                         </a-box></a-link>
 
-                        <a-link className="clickable" href="https://jonathanschwartz.surge.sh/" title="" image="" position=".8 0.0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#websiteTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
+                        <a-link class="clickable" href="https://jonathanschwartz.surge.sh/" title="" image="" position=".8 0.0 1" rotation="-67.42 0 0" scale="0.6 0.6 0.6" geometry="primitive:circle;segments:64" material="shader:portal;side:double;visible:false" link="title:."><a-box scale="0.8 0.8 1.05" material="src:#websiteTexture" position="-0.09854454977856898 -0.0036935886841299587 -0.2155814669584256" radius="2" segments-height="84" rotation="-23.450000000000003 0 0" geometry="">
                             <a-animation attribute="rotation" begin="mouseenter" loop="true" end="mouseleave" dur="2000" fill="backwards" to="0 360 0" ease="linear"></a-animation>
                         </a-box></a-link>
                     </a-marker>
 
                     <a-entity camera="" position="" rotation="" scale="" visible=""></a-entity>
-                    <a-entity cursor="fuse: true; fuseTimeout: 2000" raycaster="objects: .clickable" position="0 0 -1" scale="0.01 0.01 0.01" geometry="primitive: ring" material="color: white; shader: flat" rotation="" visible="">
+                    <a-entity cursor="fuse: false; fuseTimeout: 2000" raycaster="objects: .clickable" position="0 0 -1" scale="0.01 0.01 0.01" geometry="primitive: ring" material="color: white; shader: flat" rotation="" visible="">
                         <a-animation begin="fusing" easing="ease-in" attribute="scale" fill="backwards" from="0.01 0.01 0.01" to="0.02 0.02 0.02" dur="3000"></a-animation>
                         <a-animation begin="fusing" easing="ease-in" attribute="material.color" from="white" to="orange" dur="3000"></a-animation>
                     </a-entity>
